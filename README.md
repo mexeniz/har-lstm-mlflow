b8fd751d79cb0040ee7c297438efbcc9a0e9679c
Pawissakan Chirupphapa <pawissakan.cpcu@gmail.com>
2022-01-14 11:25:50 +0900

    Plot data by seaborn and add exploration notebook

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # har-lstm
 Human Activity Recognition with LSTM model
+
+This project follows the idea from [LSTMs for Human Activity Recognition Time Series Classification](https://machinelearningmastery.com/how-to-develop-rnn-models-for-human-activity-recognition-time-series-classification/) but implementation is done with PyTorch instead of Keras. Moreover, Docker environment and Python notebook are provided for a convenient experiment.
+
+## 1. Installation
+### 1.1 Set up environment variables
+- Create `.env` and insert these below variables. `UID` is an ID of a current user (check by running a command line `id`). `JUPYTER_CONTAINER_MEM_LIMIT` is used to limit memory usage of Jupyter-lab's container.
+```
+UID=1000
+JUPYTER_CONTAINER_MEM_LIMIT=8g
+```
+
+### 1.2 Build Docker image
+```
+docker-compose build
+```
+
+### 1.3 Download the UCI HAR dataset
+- Go to https://archive.ics.uci.edu/ml/datasets/human+activity+recognition+using+smartphones
+- https://archive.ics.uci.edu/ml/machine-learning-databases/00240/UCI%20HAR%20Dataset.zip
+- Extract files to `data` folder
+
+### 1.3 Run Docker container
+```
+```
---
+++ utils.py
@@ -0,0 +1,86 @@
+import os
+import re
+
+from loguru import logger
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import torch
+from torch.utils.data import TensorDataset, DataLoader
+
+class FeatUtils():
+    
+    @staticmethod
+    def load_file(filepath):
+        "load a single file as a dataframe"
+        df = pd.read_csv(filepath, header=None, delim_whitespace=True)
+        return df
+
+    @classmethod
+    def load_group(cls, filenames, prefix=""):
+        """load a list of files into a 3D array of [samples, timesteps, features]"""
+        data_dict = {}
+        for filename in filenames:
+            col = re.sub(r"(_train.txt)|(_test.txt)", "", filename)
+            data_dict[col] = cls.load_file(os.path.join(prefix, filename))
+        # stack group so that features are the 3rd dimension
+    #     loaded = np.dstack(loaded)
+        df = pd.concat(data_dict, axis=1)
+        return df
+
+
+    @classmethod
+    def load_dataset_group(cls, group, prefix=""):
+        """load a dataset group, such as train or test"""
+        dirpath = os.path.join(prefix, group, "Inertial Signals")
+        # load all 9 files as a single array
+        filenames = list()
+        for feat_type in ["total_acc", "body_acc", "body_gyro"]:
+            for axis in ["x", "y", "z"]:
+                filenames.append(f"{feat_type}_{axis}_{group}.txt")
+        # load input data
+        X = cls.load_group(filenames, prefix=dirpath)
+        # load class output
+        y = cls.load_file(os.path.join(prefix, group, f"y_{group}.txt"))
+        y.rename({0:"label"}, axis=1, inplace=True)
+        return X, y
+    
+    @classmethod
+    def make_train_valid_test_feature(cls, X, y, prep_func=None, norm=False, split_frac=0.8):
+        """Make features from loaded dataframes for train/validate/test"""
+        # Pre-process features here...
+        if prep_func is not None:
+            X = prep_func(X)
+        
+        # Reshape to fit with 128 steps and 9 features
+        X = np.array(X).reshape(-1, 128, 9, order="F")
+        y = np.array(y)
+
+        # Use `stratify` to preserve class distribution
+        X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=split_frac, random_state=88, stratify=y)
+
+        if norm:
+            mean, std = cls.compute_mean_std(X_train)
+
+            X_train = cls.normalize_feature(X_train, mean, std)
+            X_test = cls.normalize_feature(X_test, mean, std)
+
+        # Make validation set
+        X_train, X_valid, y_train, y_valid = train_test_split(X_train, y_train, test_size=0.3, random_state=88, stratify=y_train)
+
+        return X_train, X_valid, X_test, y_train, y_valid, y_test
+
+    @classmethod
+    def make_dataloaders(cls, X_train, X_valid, X_test, y_train, y_valid, y_test, batch_size=128):
+        # create Tensor datasets
+        train_data = TensorDataset(torch.from_numpy(X_train), torch.from_numpy(y_train))
+        valid_data = TensorDataset(torch.from_numpy(X_valid), torch.from_numpy(y_valid))
+        test_data = TensorDataset(torch.from_numpy(X_test), torch.from_numpy(y_test))
+
+        # make sure to SHUFFLE your data
+        # NOTE: Drop last to prevent a mismatched size of hidden state
+        train_loader = DataLoader(train_data, shuffle=True, batch_size=batch_size, drop_last=True)
+        valid_loader = DataLoader(valid_data, shuffle=True, batch_size=batch_size, drop_last=True)
+        test_loader = DataLoader(test_data, shuffle=True, batch_size=batch_size, drop_last=True)
+
+        return train_loader, valid_loader, test_loader
